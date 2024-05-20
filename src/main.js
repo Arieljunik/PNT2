534b8602e45a7b6cf75af4ef8ee1b03b44a3349c
SebiPochi <sebipochi@gmail.com>
2024-05-20 20:01:03 -0300

    add routing init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from './components/views/HomeView.vue'
+import LoginView from './components/views/LoginView.vue'
+import ConfirmacionView from './components/views/ConfirmacionView.vue'
+import CarritoView from './components/views/CarritoView.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    routes: [
+        {
+            path: '/',
+            component: HomeView
+        },  {
+            path: '/login',
+            component: LoginView
+        },  {
+            path: '/confirmacion',
+            component: ConfirmacionView
+        },  {
+            path: '/carrito',
+            component: CarritoView
+        }
+    ],
+    history: createWebHistory()
+})
+
+createApp(App)
+.use(router)
+.mount('#app')
