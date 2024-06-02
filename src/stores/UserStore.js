91b87bc383f9faa2fa16278121e7035654c375c2
SebiPochi <sebipochi@gmail.com>
2024-06-02 20:05:39 -0300

    funcion login y ruteo

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -3,15 +3,14 @@ import { ref, computed } from "vue";
 
 export const useUserStore = defineStore('userStore', () => {
     //State
-    const _currentUser = ref(0)
+    const _currentUser = ref({})
 
     //Getter
     const currentUser = computed(() => _currentUser.value)
 
     //Action
-    //ESTO NO FUNCIONA (login)
-    const login = (user) => {
-        _currentUser = user
+    function login (user) {
+        _currentUser.value = user
     }
     
     const isUserAutenticated = computed(() => _currentUser.value != null)
