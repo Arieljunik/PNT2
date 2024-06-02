4f33361742eb2d707560ae647582b814091ca322
SebiPochi <sebipochi@gmail.com>
2024-06-02 20:55:06 -0300

    creo navbar con conditional rendering

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav v-if="!route.meta.hideNavbar">
+        <ul>
+            <li>
+                <RouterLink to="/">Home</RouterLink> 
+            </li>
+            <li>
+                <RouterLink to="/carrito">Carrito</RouterLink>
+            </li>
+            <li>
+                <p>{{ userStore.currentUser.email }}, {{ userStore.currentUser.rol }}</p>
+            </li>
+            <li>
+                <button @click="userStore.logout()">Logout</button>
+            </li>
+        </ul>
+    </nav>
+</template>
+<script setup>
+import { RouterLink, useRoute } from 'vue-router';
+import { useUserStore } from '../stores/UserStore';
+
+const userStore = useUserStore()
+const route = useRoute()
+
+</script>
+<style>
+nav {
+    background-color: rgb(46, 46, 46);
+}
+nav ul {
+    list-style-type: none;
+}
+nav li {
+    display: inline-block;
+    padding-inline: 10px;
+}
+</style>
